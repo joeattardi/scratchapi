a83768114e88586e2806d440828498341c48ab37
Joe Attardi <jattardi@gmail.com>
2025-12-06 16:12:14 -0500

    Add shadcn components

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,5 +1,13 @@
+import { Button } from './components/ui/button';
+import { Input } from './components/ui/input';
+
 export default function App() {
     return (
-        <h1>ScratchAPI</h1>
+        <main className="p-4">
+            <div className="flex items-center gap-2">
+                <Input type="text" placeholder="Enter URL" />
+                <Button>Send</Button>
+            </div>
+        </main>
     );
 }
