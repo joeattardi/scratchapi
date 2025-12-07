bd6f5cb75c9611d2f0e08bc4d8c6dac75b3bef7f
Joe Attardi <jattardi@gmail.com>
2025-12-06 20:39:54 -0500

    Add icons

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,39 +1,48 @@
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
+import { PlayIcon } from '@phosphor-icons/react';
 import { useState } from 'react';
 
 export default function App() {
-    const [url, setUrl] = useState<string>('');
+    const [url, setUrl] = useState<string>('https://httpbin.org/get');
     const [responseBody, setResponseBody] = useState<string>('');
+    const [isLoading, setLoading] = useState(false);
 
     async function onClickSend() {
+        setLoading(true);
         const response = await window.electronAPI.sendRequest({
             url
         });
 
         setResponseBody(JSON.stringify(response, null, 2));
+        setLoading(false);
     }
 
     return (
-        <main className="p-4 flex flex-col gap-4">
-            <section>
-                <h2 className="text-lg font-bold">Request</h2>
-                <div className="flex items-center gap-2">
-                    <Input
-                        type="text"
-                        placeholder="Enter URL"
-                        value={url}
-                        onChange={(e) => setUrl(e.target.value)}
-                    />
-                    <Button onClick={onClickSend}>Send</Button>
-                </div>
-            </section>
-            <section>
-                <h2 className="text-lg font-bold">Response</h2>
-                <code>
-                    <pre>{responseBody}</pre>
-                </code>
-            </section>
-        </main>
+        <div>
+            <main className="p-4 flex flex-col gap-4">
+                <section>
+                    <h2 className="uppercase text-xs">Request</h2>
+                    <div className="flex items-center gap-2">
+                        <Input
+                            type="text"
+                            placeholder="Enter URL"
+                            value={url}
+                            onChange={(e) => setUrl(e.target.value)}
+                        />
+                        <Button onClick={onClickSend} disabled={isLoading}>
+                            <PlayIcon />
+                            Send
+                        </Button>
+                    </div>
+                </section>
+                <section>
+                    <h2 className="uppercase text-xs">Response</h2>
+                    <code>
+                        <pre>{responseBody}</pre>
+                    </code>
+                </section>
+            </main>
+        </div>
     );
 }
