d1cb1b6a9d05e7239fb8f89b05ece880ac997ca8
Joe Attardi <jattardi@gmail.com>
2025-12-07 12:31:40 -0500

    Use app icon in empty state

--- src/renderer/response/EmptyState.tsx
+++ src/renderer/response/EmptyState.tsx
@@ -1,10 +1,10 @@
-import { NetworkIcon } from '@phosphor-icons/react';
+import icon from '../images/icon.svg';
 
 export default function EmptyState() {
     return (
-        <div className="flex flex-col items-center p-4 text-zinc-500 text-sm">
-            <NetworkIcon size={128} weight="duotone" className="text-zinc-300" />
-            Nothing to show yet. Send a request to get started.
+        <div className="flex flex-col items-center gap-4 text-zinc-500 text-sm">
+            <img src={icon} alt="Network Icon" className="w-32 h-32 grayscale opacity-25" />
+            <span>Nothing to show yet. Send a request to get started.</span>
         </div>
     )
 }
